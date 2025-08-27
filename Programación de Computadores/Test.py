679584b2386839d70c80921a468287029dc4290f
josalcedog-cpu <josalcedog@unal.edu.co>
2025-08-27 12:02:52 -0500

    Add files via upload
    Ejercicios realizados en clase.

---
+++ Programación de Computadores/Test.py
@@ -0,0 +1,37 @@
+# Autor: Daniel Salcedo 
+print  ("Hello World")
+
+#Definición de problemas 
+
+
+#Solución de Problemas
+
+#Algoritmos
+
+#Encuentre el valor de x en la siguiente ecuación: 
+
+# 2x + 3 = 7
+# 2x = 7-3 
+# 2x = 4
+#(2x)/2 = 4/2 
+# x = 2
+
+#x = print ("Hello")
+# print (x) ; no de vuelve nada al ser x una variable nula (print no devuelve datos)
+#print (type(x))
+x1= 7 - 3 
+x2 = x1 // 2 # Si opero una división con / me devolverá un float y si lo hago con un // me devolverá un entero 
+print(type(x1), x1)
+print(type(x2), x2)
+print("El valor de x es ", x2)
+mensaje = " El valor de la varieble es : "
+print(type(mensaje), mensaje)
+print(mensaje, x2)
+
+print ("==========================================================================================================")
+
+#Encuentre el valor de x en la siguiente ecuación: 
+
+#AX = B- Z
+A = input("--->")
+print (A)
