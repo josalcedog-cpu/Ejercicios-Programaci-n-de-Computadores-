5e4c81ae6b2e23cfefd3c6d4fcd7a756a110f6e7
josalcedog-cpu <josalcedog@unal.edu.co>
2025-08-27 12:52:17 -0500

    Add files via upload

---
+++ Programación de Computadores/archivo.py
@@ -0,0 +1,26 @@
+# Autor: Daniel Salcedo 
+# Este programa es un ejemplo de instrucciones basicas
+
+
+#Sumas, restas, multiplicacion, division, potenciacion, radicacion
+x1 = 10 + 5 
+x2 = 10- 5
+x3 = 10*5 
+x4 = 10/5
+x5 = 10**5
+x6 = 25**0.5
+
+print("----------------------------------------------------------------------")
+
+print ("El resultado de la suma 10  + 5 es igual a", x1)
+print ("El resultado de la resta 10  - 5 es igual a", x2)
+print ("El resultado de la multiplicación  10 x 5 es igual a", x3)
+print ("El resultado de la division 10 / 5 es igual a", x4)
+print ("La quinta potencia de 10 es igual a ", x5)
+print ("La raiz cuadrada de 25 es igual a ", x6)
+
+print("----------------------------------------------------------------------")
+#la funcion print, texto, con variables, concatenando
+print ("Hola Mundo")
+print ("la raiz cuadrada de veinticinco es", x6, " y si le sumamos 10 nos da", x1 )
+print("----------------------------------------------------------------------")
